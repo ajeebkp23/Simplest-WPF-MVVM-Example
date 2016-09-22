29c6117cd4919d52ebcd2ac1829a051da127958b
Ajeeb <ajeebkp23@gmail.com>
2016-09-22 16:10:14 +0530

    A example shows MVVM with Simple WPF

---
+++ WPF MVVM Quick Start Tutorial/View/TextConverter.cs
@@ -0,0 +1,21 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_Quick_Start_Tutorial
+{
+    public class TextConverter
+    {
+        private readonly Func<string, string> _converstion;
+        public TextConverter(Func<string,string> conversion)
+        {
+            _converstion = conversion;
+        }
+        public string ConvertText(string inputText)
+        {
+            return _converstion(inputText);
+        }
+    }
+}
---
+++ WPF MVVM Quick Start Tutorial/ViewModel/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Quick_Start_Tutorial
+{
+    public class ViewModel: ObservableObject
+    {
+        private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+        private void ConvertText()
+        {
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = String.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
