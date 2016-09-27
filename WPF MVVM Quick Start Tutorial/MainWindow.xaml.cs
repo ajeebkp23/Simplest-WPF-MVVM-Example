46d4d179bb4df4910cd480a529577ff63bf8c559
Ajeeb <ajeebkp23@gmail.com>
2016-09-27 16:17:14 +0530

    Partial working code

--- WPF MVVM Quick Start Tutorial/MainWindow.xaml.cs
+++ WPF MVVM Quick Start Tutorial/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,63 @@ namespace WPF_MVVM_Quick_Start_Tutorial
         public MainWindow()
         {
             InitializeComponent();
+
+            ModbusNode tcpNode = new ModbusNode("ModbusTCP");
+            ModbusNode rtuNode = new ModbusNode("ModbusRTU");
+
+            tcpNode.PortNodes = new ObservableCollection<ModbusPortNode> { new ModbusPortNode("11"), new ModbusPortNode("22") };
+
+            RealTimeDataNode rootNode = new RealTimeDataNode(new ObservableCollection<ModbusNode> { tcpNode, rtuNode });
+            DataSourcesTreeView.DataContext = rootNode;
         }
     }
+
+
+    public class ModbusPortNode
+    {
+        private string portName;
+
+        public string PortName
+        {
+            get { return portName; }
+            set { portName = value; }
+        }
+
+        public ModbusPortNode(string Name)
+        {
+            portName = Name;
+        }
+    }
+
+    public class ModbusNode
+    {
+        public string DisplayName { get; set; }
+        public ModbusNode(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        private ObservableCollection<ModbusPortNode> portNodes;
+        public ObservableCollection<ModbusPortNode> PortNodes
+        {
+            get { return portNodes; }
+            set { portNodes = value; }
+        }
+    }
+
+    public class RealTimeDataNode
+    {
+        private ObservableCollection<ModbusNode> rTSources;
+        public ObservableCollection<ModbusNode> RTSources
+        {
+            get { return rTSources; }
+            set { rTSources = value; }
+        }
+
+        public RealTimeDataNode(ObservableCollection<ModbusNode> sources)
+        {
+            rTSources = sources;
+        }
+    }
+
 }
